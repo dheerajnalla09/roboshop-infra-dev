1b0641fdd0ea9f584474ef78dd80c33d46a51675
Dheeraj <dheeraj.azure@aol.com>
2026-03-13 12:30:29 +0530

    initial commit

---
+++ 30-bastion/main.tf
@@ -0,0 +1,60 @@
+resource "aws_instance" "bastion" {
+  ami                    = local.ami_id
+  instance_type          = "t3.micro"
+  subnet_id              = local.public_subnet_id
+  vpc_security_group_ids = [local.bastion_sg_id]
+  iam_instance_profile   = aws_iam_instance_profile.bastion.name
+
+  root_block_device {
+    volume_size = 50
+    volume_type = "gp3"
+
+    tags = merge(
+      {
+        Name = "${var.project}-${var.environment}-bastion"
+      },
+      local.common_tags
+    )
+  }
+
+  tags = merge(
+    {
+      Name = "${var.project}-${var.environment}-bastion"
+    },
+    local.common_tags
+  )
+}
+
+resource "aws_iam_role" "bastion" {
+  name = "RoboShopDevBastion"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = merge(
+    {
+      Name = "RoboShopDevBastion"
+    },
+    local.common_tags
+  )
+}
+
+resource "aws_iam_role_policy_attachment" "bastion" {
+  role       = aws_iam_role.bastion.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_instance_profile" "bastion" {
+  name = "${var.project}-${var.environment}-bastion"
+  role = aws_iam_role.bastion.name
+}
