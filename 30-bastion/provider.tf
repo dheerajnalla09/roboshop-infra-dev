6812bc17a005fecf59cd50aaa5ba23cc82682782
Dheeraj <dheeraj.azure@aol.com>
2026-03-13 18:25:08 +0530

    bastion terraform code added

--- 30-bastion/provider.tf
+++ 30-bastion/provider.tf
@@ -1,17 +1,17 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "6.33.0" # Terraform AWS provider version
+      source  = "hashicorp/aws"
+      version = "6.33.0"
     }
   }
 
   backend "s3" {
-    bucket  = "remote-state-dheeraj" # Replace with your unique bucket name
-    key     = "roboshop-dev-sg-rules"
+    bucket  = "remote-state-dheeraj"
+    key     = "roboshop-dev-bastion"
     region  = "us-east-1"
     encrypt = true
-    use_lockfile   = true
+    use_lockfile = true
   }
 }
 
--- 50-backend-db/variables.tf
+++ 50-backend-db/variables.tf
@@ -5,3 +5,11 @@ variable "project" {
 variable "environment" {
     default = "dev"
 }
+
+variable "zone_id" {
+    default = "Z0493894GT35YUX5U0H3"
+}
+
+variable "domain_name" {
+    default = "dheerajn.online"
+}
