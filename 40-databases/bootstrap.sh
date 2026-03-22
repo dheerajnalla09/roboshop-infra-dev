c914248a066b02f03e2c09a2d1c8cf2dc98cc4a8
Dheeraj <dheeraj.azure@aol.com>
2026-03-22 17:02:22 +0530

    Fix line endings

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
+#!/bin/bash
+
+set -e   # ✅ stop on error
+
 component=$1
 environment=$2
-dnf install ansible -y
+
+echo "Starting bootstrap for $component in $environment"
+
+# Install ansible only if not present
+if ! command -v ansible &> /dev/null
+then
+    echo "Installing Ansible..."
+    dnf install ansible -y
+fi
 
 cd /home/ec2-user
-git clone http://github.com/dheerajnalla09/ansible-roboshop-roles-tf.git
 
+# Clone only if repo not exists
+if [ ! -d "ansible-roboshop-roles-tf" ]; then
+    git clone https://github.com/dheerajnalla09/ansible-roboshop-roles-tf.git
+fi
 
 cd ansible-roboshop-roles-tf
 git pull
+
+# Run playbook
 ansible-playbook -e component=$component -e env=$environment roboshop.yaml
+
+echo "Bootstrap completed for $component"
