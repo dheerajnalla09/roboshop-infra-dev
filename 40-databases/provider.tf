9b64cbd22d90d7530dec2708a4174f4d238b8d01
Dheeraj <dheeraj.azure@aol.com>
2026-03-22 11:26:38 +0530

    Added infra folders

--- 40-databases/provider.tf
+++ 40-databases/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket  = "remote-state-dheeraj" # Replace with your unique bucket name
-    key     = "roboshop-dev-sg-rules"
+    key     = "roboshop-dev-db"
     region  = "us-east-1"
     encrypt = true
     use_lockfile   = true
